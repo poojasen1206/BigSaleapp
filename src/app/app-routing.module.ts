3f691d492763dc88703cfc709b102be4915b66d8
pooja1206 <sen.pooja94@gmail.com>
2020-09-09 19:49:51 +0530

    add router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { UploadItemsComponent } from './upload-items/upload-items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo:'Home', pathMatch: 'full'
+   },
+   {
+    path:'Home', component:HomeComponent
+  },
+  {
+    path:'about-us', component:AboutUsComponent
+  },
+  {
+    path:'upload-items', component:UploadItemsComponent
+  },
+  {
+    path:'contact-us', component:ContactUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
